6ee0d67ffaf9222652642ce449df33a786dd2aba
Kaliyappan <balamurugan.kaliyappan@cgi.com>
2024-11-25 15:28:45 +0530

    commit change

---
+++ src/main/java/com/select.java
@@ -0,0 +1,17 @@
+package com;
+
+
+import java.util.List;  // Unused import that will trigger a warning in Codacy
+
+public class select {
+    public static void main(String[] args) {
+        // Hardcoded value - Codacy might suggest using constants instead
+        String greeting = "Hello world!";
+        
+        // Potential inefficiency - using a non-optimal way to concatenate strings
+        String message = greeting + " Let's learn coding."; 
+
+        System.out.println(message);  // This will display the concatenated string
+    }
+}
+
